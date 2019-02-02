f7d7410688c9a9bae76f16d3e29c7636fb4b752d
Wojciech <w.wasilewski@gmail.com>
2019-02-02 12:49:18 +0100

    adding ex6

--- src/main/java/exam/ex6/Main6.java
+++ src/main/java/exam/ex6/Main6.java
@@ -1,4 +1,15 @@
+//W pliku `Main6.java` umieść metodę o sygnaturze `static String replaceStr(String str, String forReplace, String replacement)`.
+//1. Uzupełnij ciało metody tak aby zwracała napis powstały z zamiany w napisie `str`
+// wszystkich wystąpień napisu `forReplace` przez napis `replacement`.
+
 package exam.ex6;
 
 public class Main6 {
+    public static void main(String[] args) {
+
+        Zad6 zad6 = new Zad6();
+        System.out.println("poczatkowy napis: \"string bedzie zamieniony\"");
+        System.out.println("po zamianie \"bedzie\" na \"jest\": "
+                + zad6.replaceStr("string bedzie zamieniony", "bedzie", "jest"));
+    }
 }
---
+++ src/main/java/exam/ex6/Zad6.java
@@ -0,0 +1,12 @@
+//W pliku `Main6.java` umieść metodę o sygnaturze `static String replaceStr(String str, String forReplace, String replacement)`.
+//Uzupełnij ciało metody tak aby zwracała napis powstały z zamiany w napisie `str`
+// wszystkich wystąpień napisu `forReplace` przez napis `replacement`.
+
+package exam.ex6;
+
+public class Zad6 {
+
+    public String replaceStr(String str, String forReplace, String replacement) {
+        return str.replace(forReplace, replacement);
+    }
+}
